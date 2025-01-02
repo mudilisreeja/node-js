d503ec448496a9477135be1f80f64012212eb6da
sreeja <sreeja.mudili@gmail.com>
2025-01-02 11:55:14 +0530

    module exports

---
+++ p1.js
@@ -0,0 +1,16 @@
+//module wrapper function
+//function(exports,require,module,__filename,__dirname){
+//}
+
+console.log(__dirname,__filename);
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greeting() {
+        console.log(`My name is ${this.name} and my age is ${this.age}`);
+    }
+}
+
+module.exports = Person;

5402feffcd07bc8f41c73895abfb9211a2a51e28
sreeja <sreeja.mudili@gmail.com>
2025-01-06 15:48:33 +0530

    Event emitter

---
+++ logger.js
@@ -0,0 +1,9 @@
+const EventEmitter=require('events');
+const uuid=require('uuid');
+class logger extends EventEmitter{
+    log(msg){
+        //call event
+        this.emit(`message`,{id:uuid.v4,msg});
+    }
+}
+module.exports=logger;

a7052cdee42bbe5d31cbb727736781a726725648
sreeja <sreeja.mudili@gmail.com>
2025-01-06 11:40:04 +0530

    os modules

---
+++ os_modules.js
@@ -0,0 +1,16 @@
+const os=require('os');
+//os platform
+console.log(os.platform());
+//cpu arch
+console.log(os.arch());
+//cpu core info
+console.log(os.cpus());
+//free memory
+console.log(os.freemem());
+//total memory
+console.log(os.totalmem());
+//home directory
+console.log(os.homedir());
+//up time
+console.log(os.uptime());
+
